ddbbdc065badb265bf39129a36628666e5c10cbc
lvtoness <88632356+Wizardkkj@users.noreply.github.com>
2023-08-16 06:37:01 -0300

    Create correctpath.lua

---
+++ correctpath.lua
@@ -0,0 +1,269 @@
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "1",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "2",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "3",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "4",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "5",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "6",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "7",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
+
+
+
+
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+
+
+ESP.Players = false
+
+ESP.Boxes = true
+
+ESP.Names = true
+
+ESP:Toggle(true)
+
+
+
+ESP:AddObjectListener(Workspace.Challenge.GraveGoons.CorrectPath, {
+
+    Name = "8",
+
+
+
+    CustomName = "this way",
+
+    Color = Color3.fromRGB(51, 255, 51),
+
+    IsEnabled = "luvesp"
+
+})
+
+ESP.luvesp = true
